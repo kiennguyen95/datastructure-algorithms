ac3315fca5b2577027edebdd3f13f20572e813d4
Kien Nguyen <henry@offspringdigital.com>
2021-01-19 15:58:51 +0700

    Implement binary tree traversals

---
+++ tree_traversals_implement.py
@@ -0,0 +1,141 @@
+class TreeNode():
+
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def preOrderTraversalRecursive(root: TreeNode):
+    res = []
+    if root:
+        res.append(root.data)
+        res += preOrderTraversalRecursive(root.left)
+        res += preOrderTraversalRecursive(root.right)
+    return res
+
+
+def preOrderTraversalIterative(root: TreeNode):
+    if not root:
+        return []
+
+    res = []
+    stack = [root]
+    while stack:
+        node = stack.pop()
+        res.append(node.data)
+        if node.right:
+            stack.append(node.right)
+        if node.left:
+            stack.append(node.left)
+    return res
+
+
+def inOrderTraversalRecursive(root: TreeNode):
+    res = []
+    if root:
+        res += inOrderTraversalRecursive(root.left)
+        res.append(root.data)
+        res += inOrderTraversalRecursive(root.right)
+    return res
+
+
+def inOrderTraversalIterative(root: TreeNode):
+    if not root:
+        return []
+
+    res = []
+    stack = []
+    current = root
+    while stack or current:
+        if current:
+            stack.append(current)
+            current = current.left
+        else:
+            node = stack.pop()
+            res.append(node.data)
+            if node.right:
+                current = node.right
+    return res
+
+
+def postOrderTraversalRecursive(root: TreeNode):
+    res = []
+    if root:
+        res += postOrderTraversalRecursive(root.left)
+        res += postOrderTraversalRecursive(root.right)
+        res.append(root.data)
+    return res
+
+
+# 1.1 Create an empty stack
+# 2.1 Do following while root is not NULL
+#     a) Push root's right child and then root to stack.
+#     b) Set root as root's left child.
+# 2.2 Pop an item from stack and set it as root.
+#     a) If the popped item has a right child and the right child
+#        is at top of stack, then remove the right child from stack,
+#        push the root back and set root as root's right child.
+#     b) Else print root's data and set root as NULL.
+# 2.3 Repeat steps 2.1 and 2.2 while stack is not empty.
+def postOrderTraversalIterative(root: TreeNode):
+    if not root:
+        return []
+
+    res = []
+    stack = []
+    while True:
+        while root:
+            if root.right is not None:
+                stack.append(root.right)
+            stack.append(root)
+            root = root.left
+        root = stack.pop()
+        if (root.right is not None and stack and stack[-1] == root.right):
+            stack.pop()
+            stack.append(root)
+            root = root.right
+        else:
+            res.append(root.data)
+            root = None
+        if (len(stack) <= 0):
+            break
+    return res
+
+import unittest
+
+
+class TestTreeTraversals(unittest.TestCase):
+
+    tree: TreeNode
+
+    def setUp(self):
+        root = TreeNode(8)
+        root.left = TreeNode(3)
+        root.left.left = TreeNode(1)
+        root.left.right = TreeNode(6)
+        root.left.right.left = TreeNode(4)
+        root.left.right.right = TreeNode(7)
+        root.right = TreeNode(10)
+        root.right.right = TreeNode(14)
+        root.right.right.left = TreeNode(13)
+        self.tree = root
+
+
+    def testPreOrderTraversal(self):
+        self.assertEqual(preOrderTraversalRecursive(self.tree), [8, 3, 1, 6, 4, 7, 10, 14, 13])
+        self.assertEqual(preOrderTraversalIterative(self.tree), [8, 3, 1, 6, 4, 7, 10, 14, 13])
+
+
+    def testInOrderTraversal(self):
+        self.assertEqual(inOrderTraversalRecursive(self.tree), [1, 3, 4, 6, 7, 8, 10, 13, 14])
+        self.assertEqual(inOrderTraversalIterative(self.tree), [1, 3, 4, 6, 7, 8, 10, 13, 14])
+
+
+    def testPostOrderTraversal(self):
+        self.assertEqual(postOrderTraversalRecursive(self.tree), [1, 4, 7, 6, 3, 13, 14, 10, 8])
+        self.assertEqual(postOrderTraversalIterative(self.tree), [1, 4, 7, 6, 3, 13, 14, 10, 8])
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
